90f403276fb813162d27ba9818ee9473759d92de
avivnaaman <34813756+avivnaaman@users.noreply.github.com>
2017-12-24 16:51:41 +0200

    Add files via upload

---
+++ GlobalingHTML.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for GlobalHTMLDefaults
+/// </summary>
+namespace GlobalHTML
+{
+    public class GlobalingHTMLNavBar
+    {
+        static GlobalingHTMLNavBar() { GlobalHTMLNavBar = "<a href='../../../' class='NavBarItem NavBarButton'>בית</a><a href='../../Lessons.aspx' class='NavBarItem NavBarButton'>למד</a><a href='../../reference.aspx' class='NavBarItem NavBarButton'>רשימות</a><a href='../../contact.aspx' class='NavBarItem NavBarButton'>צור קשר</a>"; } // default value
+        public static string GlobalHTMLNavBar { get; private set; }
+    }
+    public class GlobalingHTMLFooter
+    {
+        static GlobalingHTMLFooter() { GlobalHTMLFooter = ""; }
+        public static string GlobalHTMLFooter { get; private set; }
+    }
+    //TODO: Make New DB access that will request the HTML code.
+    public class GlobalingHTMLNavBarHTML
+    {
+        static GlobalingHTMLNavBarHTML() { GlobalHTMLNavBarHTML = "<a href='../../../' class='NavBarItem NavBarButton'>Home</a><a href='../../Lessons.aspx' class='NavBarItem NavBarButton'>למד</a><a href='../../reference.aspx' class='NavBarItem NavBarButton'>ספח</a>"; } // default value
+        public static string GlobalHTMLNavBarHTML { get; private set; }
+    }
+    public class GlobalingHTMLNavBarMobile
+    {
+        static GlobalingHTMLNavBarMobile() { GlobalHTMLNavBarMobile = "<li><a href='../' class='linkButton'>Home</a></li><li><a href='lessons.aspx' class='linkButton'>Lessons</a></li><li><a href='reference.aspx' class='linkButton'>refrence</a></li>"; } // default value
+        public static string GlobalHTMLNavBarMobile { get; private set; }
+    }
+    public class GlobalingHTMLNavBarMobileAdmin
+    {
+        static GlobalingHTMLNavBarMobileAdmin() { GlobalHTMLNavBarMobileAdmin = "<li><a href='../' class='linkButton'>Home</a></li><li><a href='lessons.aspx' class='linkButton'>Lessons</a></li><li><a href='reference.aspx' class='linkButton'>refrence</a></li><li><a href='../UserManagment.aspx' class='linkButton'>Managment</a></li>"; } // default value
+        public static string GlobalHTMLNavBarMobileAdmin { get; private set; }
+    }
+    public class GlobalingHTMLNavBarMobileless
+    {
+        static GlobalingHTMLNavBarMobileless() { GlobalHTMLNavBarMobileless = "<li><a href='../../mobile/' class='linkButton'>Home</a></li><li><a href='../../mobile/lessons.aspx' class='linkButton'>Lessons</a></li><li><a href='../../mobile/reference.aspx' class='linkButton'>refrence</a></li>"; } // default value
+        public static string GlobalHTMLNavBarMobileless { get; private set; }
+    }
+    public class GlobalingHTMLNavBarMobileAdminless
+    {
+        static GlobalingHTMLNavBarMobileAdminless() { GlobalHTMLNavBarMobileAdminless = "<li><a href='../../mobile/' class='linkButton'>Home</a></li><li><a href='../../mobile/lessons.aspx' class='linkButton'>Lessons</a></li><li><a href='../../mobile/reference.aspx' class='linkButton'>refrence</a></li><li><a href='../../UserManagment.aspx' class='linkButton'>Managment</a></li>"; } // default value
+        public static string GlobalHTMLNavBarMobileAdminless { get; private set; }
+    }
+}
---
+++ Globals.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Globals
+/// </summary>
+namespace GlobalVariables
+{
+    public class GlobalingRegisterSignOutMessage
+    {
+        // parameterless constructor required for static class
+        static GlobalingRegisterSignOutMessage() { GlobalRegisterSignOutMessage = null; } // default value
+
+        // public get, and private set for strict access control
+        public static string GlobalRegisterSignOutMessage { get; private set; }
+
+        // GlobalInt can be changed only via this method
+        public static void SetGlobalRegisterSignOutMessageValue(string newString)
+        {
+            GlobalRegisterSignOutMessage = newString;
+        }
+    }
+    public class GlobalingVr
+    {
+        static GlobalingVr() { GlobalVr = "2.6"; }
+        public static string GlobalVr { get; private set; }
+    }
+    public class GlobalingRedirectedFrom
+    {
+        static GlobalingRedirectedFrom() { GlobalRedirectedFrom = null; }
+        public static string GlobalRedirectedFrom { get; set; }
+        public static void SetGlobalRedirectedFromValue(string newString)
+        {
+            GlobalRedirectedFrom = newString;
+        }
+    }
+}
